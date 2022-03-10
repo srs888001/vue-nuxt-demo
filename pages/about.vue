4d0d81aaa97f2d4d3f9e1246302525713a28d4c4
Jerry <sirusheng@soulapp.cn>
2022-03-10 23:45:53 +0800

    路由导航

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
      <h1>About Page</h1> 
+     <!-- a链接 刷新 服务端渲染-->
+     <h1>a链接</h1> 
+     <a href="/"> 首页 </a>
+
+     <!-- router-link 导航链接组件 -->
+     <h1>router-lin</h1> 
+     <router-link to="/">  首页 </router-link>
+
+     <!-- 编程式导航 -->
+     <h1>编程式导航</h1> 
+     <button @click="onClick">  首页 </button>
   </div>
 </template>
  
 <script>
 export default {
-    name: "AboutPage"
+    name: "AboutPage",
+    methods: {
+        onClick() {
+            this.$router.push('/')
+        }
+    }
 }
 </script>
 
